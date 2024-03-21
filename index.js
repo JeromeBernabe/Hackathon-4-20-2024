e773462c589299a52344a527b2144e40f31b1300
unknown <jerbz1122@gmail.com>
2024-03-21 10:40:03 +0800

    Ikaw bahala ba't ako ang tinatanong mo gago

---
+++ index.js
@@ -0,0 +1,39 @@
+let age = window.prompt("how old are you?")
+age = Number(age);
+age+=1;
+
+document.getElementById("greet").textContent = `Hello ${age}`;
+
+
+
+// let username
+
+// document.getElementById("mySubmit").onclick = function(){
+//     username = document.getElementById("myText").value;
+
+//     document.getElementById("greet").textContent = `Hello ${username}`;
+// }
+
+
+
+// let username = window.prompt("what's your username?");
+
+// window.alert(`Hello ${username}`);
+
+// let x = 7 * 5;
+// let y = x - 2.1;
+// let z = "Hello";
+// const f = 9;
+// let g = true;
+// console.log(`You are ${x + 9 + y} and ${z}, this statement is ${g}`);
+
+// console.log("Hello World");
+// console.log(7*5);
+
+// // window.alert("This is an alert");
+// // window.alert("JavaScript Shit");
+// // window.alert("it looks awfully similar");
+
+// //comments
+
+// document.getElementById("h1").textContent= "Fuck u";
